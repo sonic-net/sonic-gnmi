ff851d9f42d318ce455697aa7961c95c9ebc9818
ganglyu <ganglv@microsoft.com>
2022-06-18 15:59:53 +0800

    minor update

--- gnmi_dump/gnmi_dump.go
+++ gnmi_dump/gnmi_dump.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/sonic-net/sonic-gnmi/common_utils"
 )
 
+const help = `
+gnmi_dump is used to dump internal counters for debugging purpose, including GNMI request counter, GNOI request counter and DBUS request counter.
+`
+
 func main() {
+	flag.Usage = func() {
+		fmt.Print(help)
+	}
+	flag.Parse()
 	var counters [len(common_utils.CountersName)]uint64
 	err := common_utils.GetMemCounters(&counters)
 	if err != nil {
