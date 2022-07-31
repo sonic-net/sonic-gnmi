21cc55fa4843dca5722a33d41b6ae356719471ff
Renuka Manavalan <remanava@microsoft.com>
2022-07-31 00:21:02 +0000

    compile fix

--- sonic_data_client/events_client.go
+++ sonic_data_client/events_client.go
@@ -5,6 +5,7 @@ package client
 #cgo LDFLAGS: -lpthread -lboost_thread -lboost_system -lzmq -lboost_serialization -luuid -lswsscommon
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbol.h>
 #include "events_wrap.h"
 */
 import "C"
