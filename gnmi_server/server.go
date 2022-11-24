de2504989a0a84952a66e3d0afb7fb6b6c9371a0
ganglyu <ganglv@microsoft.com>
2022-11-24 01:15:52 +0000

    Fix LGTM alert.

--- gnmi_server/server.go
+++ gnmi_server/server.go
@@ -324,7 +324,6 @@ func (s *Server) Get(ctx context.Context, req *gnmipb.GetRequest) (*gnmipb.GetRe
 
 	paths := req.GetPath()
 	extensions := req.GetExtension()
-	target = prefix.GetTarget()
 	log.V(5).Infof("GetRequest paths: %v", paths)
 
 	var dc sdc.Client
