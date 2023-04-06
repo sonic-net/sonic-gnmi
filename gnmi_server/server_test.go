aa6d0ca039c7bae2f1a6a58f2e1ac95838bb8e9d
zbud-msft <zainbudhwani@microsoft.com>
2023-04-06 21:14:19 +0000

    Fix compile

--- gnmi_server/server_test.go
+++ gnmi_server/server_test.go
@@ -2834,31 +2834,33 @@ func TestClientConnections(t *testing.T) {
 	updates   []tablePathValue 
 	wantNoti  []client.Notification
     }{
-         desc: "stream query for table key Ethernet* with new test_field field on Ethernet68",
-         q:    createCountersDbQueryOnChangeMode(t, "COUNTERS", "Ethernet*"),
-         updates: []tablePathValue{
-             {
-                 dbName:    "COUNTERS_DB",
-                 tableName: "COUNTERS",
-                 tableKey:  "oid:0x1000000000039", // "Ethernet68": "oid:0x1000000000039",
-                 delimitor: ":",
-                 field:     "test_field",
-                 value:     "test_value",
+         {
+             desc: "stream query for table key Ethernet* with new test_field field on Ethernet68",
+             q:    createCountersDbQueryOnChangeMode(t, "COUNTERS", "Ethernet*"),
+             updates: []tablePathValue{
+                 {
+                     dbName:    "COUNTERS_DB",
+                     tableName: "COUNTERS",
+                     tableKey:  "oid:0x1000000000039", // "Ethernet68": "oid:0x1000000000039",
+                     delimitor: ":",
+                     field:     "test_field",
+                     value:     "test_value",
+                 },
+                 { //Same value set should not trigger multiple updates
+                     dbName:    "COUNTERS_DB",
+                     tableName: "COUNTERS",
+                     tableKey:  "oid:0x1000000000039", // "Ethernet68": "oid:0x1000000000039",
+                     delimitor: ":",
+                     field:     "test_field",
+                     value:     "test_value",
+                 },
              },
-             { //Same value set should not trigger multiple updates
-                 dbName:    "COUNTERS_DB",
-                 tableName: "COUNTERS",
-                 tableKey:  "oid:0x1000000000039", // "Ethernet68": "oid:0x1000000000039",
-                 delimitor: ":",
-                 field:     "test_field",
-                 value:     "test_value",
+             wantNoti: []client.Notification{
+                 client.Connected{},
+                 client.Update{Path: []string{"COUNTERS", "Ethernet*"}, TS: time.Unix(0, 200), Val: countersEthernetWildcardJson}, client.Sync{},
+                 client.Update{Path: []string{"COUNTERS", "Ethernet*"}, TS: time.Unix(0, 200), Val: countersEtherneWildcardJsonUpdate},
              },
-         },
-         wantNoti: []client.Notification{
-             client.Connected{},
-             client.Update{Path: []string{"COUNTERS", "Ethernet*"}, TS: time.Unix(0, 200), Val: countersEthernetWildcardJson}, client.Sync{},
-             client.Update{Path: []string{"COUNTERS", "Ethernet*"}, TS: time.Unix(0, 200), Val: countersEtherneWildcardJsonUpdate},
-         },
+        }
     }
 
     for _, tt := range tests {
