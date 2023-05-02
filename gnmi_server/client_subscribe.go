9207875f827df98d84558821298519c2d3a38b7b
zbud-msft <zainbudhwani@microsoft.com>
2023-05-02 19:46:02 +0000

    Improve UT

--- gnmi_server/client_subscribe.go
+++ gnmi_server/client_subscribe.go
@@ -54,7 +54,7 @@ func (c *Client) setLogLevel(lvl int) {
 }
 
 func (c *Client) setConnectionManager(threshold int) {
-	if connectionManager != nil {
+	if connectionManager != nil && threshold == connectionManager.GetThreshold() { // test scenario where threshold is changed dynamically
 		return
 	}
 	connectionManager = &ConnectionManager {
