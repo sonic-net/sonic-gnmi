b478159446eb0122892940c89dba17de74d85e56
Zain Budhwani <99770260+zbud-msft@users.noreply.github.com>
2023-05-03 14:59:04 -0700

    Cherry pick commit 4a8964a (#104)

--- gnmi_server/server.go
+++ gnmi_server/server.go
@@ -296,7 +296,7 @@ func (s *Server) Get(ctx context.Context, req *gnmipb.GetRequest) (*gnmipb.GetRe
 	paths := req.GetPath()
 	extensions := req.GetExtension()
 	target = prefix.GetTarget()
-	log.V(5).Infof("GetRequest paths: %v", paths)
+	log.V(2).Infof("GetRequest paths: %v", paths)
 
 	var dc sdc.Client
 
