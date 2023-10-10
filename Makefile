sonic-net/sonic-gnmi

commit 58a7b20b760561dd4d1156e1e91d7addc434dc39

Add delete field to On change response when key is deleted (#139)

proto/sonic_internal.proto
@@ -34,4 +34,7 @@ message Value {
 
   // Notification to be used in place of 1-4 if present
   gnmi.Notification notification = 7;
+
+  // Delete to be used to indicate that node was deleted
+  []gnmi.Path delete = 8;
 }

gnmi_server/server_test.go
@@ -474,6 +474,14 @@ func prepareStateDb(t *testing.T, namespace string) {
 	defer rclient.Close()
 	rclient.FlushDB()
 	rclient.HSet("SWITCH_CAPABILITY|switch", "test_field", "test_value")
+	fileName := "../testdata/NEIGH_STATE_TABLE.txt"
+	neighStateTableByte, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		t.Fatalf("read file %v err: %v", fileName, err)
+	}
+	mpi_neigh := loadConfig(t, "", neighStateTableByte)
+	loadDB(t, rclient, mpi_neigh)
+
 }
 
 func prepareDb(t *testing.T, namespace string) {
@@ -657,6 +665,19 @@ func createEventsQuery(t *testing.T, paths ...string) client.Query {
 	return createQueryOrFail(t,
 		pb.SubscriptionList_STREAM,
 		"EVENTS",
+		[]subscriptionQuery{
+			{
+				Query:   paths,
+				SubMode: pb.SubscriptionMode_ON_CHANGE,
+			},
+		},
+		false)
+}
+
+func createStateDbQueryOnChangeMode(t *testing.T, paths ...string) client.Query {
+	return createQueryOrFail(t,
+	        pb.SubscriptionList_STREAM,
+		"STATE_DB",
 		[]subscriptionQuery{
 			{
 				Query:   paths,
@@ -2953,6 +2974,136 @@ func TestAuthCapabilities(t *testing.T) {
 	if len(resp.SupportedModels) == 0 {
 		t.Fatalf("No Supported Models found!")
 	}
+}
+
+func TestTableKeyOnDeletion(t *testing.T) {
+    s := createKeepAliveServer(t, 8081)
+    go runServer(t, s)
+    defer s.s.Stop()
+
+    fileName := "../testdata/NEIGH_STATE_TABLE_MAP.txt"
+    neighStateTableByte, err := ioutil.ReadFile(fileName)
+    if err != nil {
+        t.Fatalf("read file %v err: %v", fileName, err)
+    }
+    var neighStateTableJson interface{}
+    json.Unmarshal(neighStateTableByte, &neighStateTableJson)
+
+    fileName = "../testdata/NEIGH_STATE_TABLE_key_deletion_57.txt"
+    neighStateTableDeletedByte57, err := ioutil.ReadFile(fileName)
+    if err != nil {
+        t.Fatalf("read file %v err: %v", fileName, err)
+    }
+    var neighStateTableDeletedJson57 interface{}
+    json.Unmarshal(neighStateTableDeletedByte57, &neighStateTableDeletedJson57)
+
+    fileName = "../testdata/NEIGH_STATE_TABLE_MAP_2.txt"
+    neighStateTableByteTwo, err := ioutil.ReadFile(fileName)
+    if err != nil {
+        t.Fatalf("read file %v err: %v", fileName, err)
+    }
+    var neighStateTableJsonTwo interface{}
+    json.Unmarshal(neighStateTableByteTwo, &neighStateTableJsonTwo)
+
+    fileName = "../testdata/NEIGH_STATE_TABLE_key_deletion_59.txt"
+    neighStateTableDeletedByte59, err := ioutil.ReadFile(fileName)
+    if err != nil {
+        t.Fatalf("read file %v err: %v", fileName, err)
+    }
+    var neighStateTableDeletedJson59 interface{}
+    json.Unmarshal(neighStateTableDeletedByte59, &neighStateTableDeletedJson59)
+
+    fileName = "../testdata/NEIGH_STATE_TABLE_key_deletion_61.txt"
+    neighStateTableDeletedByte61, err := ioutil.ReadFile(fileName)
+    if err != nil {
+        t.Fatalf("read file %v err: %v", fileName, err)
+    }
+    var neighStateTableDeletedJson61 interface{}
+    json.Unmarshal(neighStateTableDeletedByte61, &neighStateTableDeletedJson61)
+
+    namespace := sdcfg.GetDbDefaultNamespace()
+    rclient := getRedisClientN(t, 6, namespace)
+    defer rclient.Close()
+    prepareStateDb(t, namespace)
+
+    tests := []struct {
+        desc      string
+        q         client.Query
+        wantNoti  []client.Notification
+        paths     []string
+    }{
+        {
+            desc: "Testing deletion of NEIGH_STATE_TABLE:10.0.0.57",
+            q: createStateDbQueryOnChangeMode(t, "NEIGH_STATE_TABLE"),
+            wantNoti: []client.Notification {
+                client.Update{Path: []string{"NEIGH_STATE_TABLE"}, TS: time.Unix(0, 200), Val: neighStateTableJson},
+                client.Update{Path: []string{"NEIGH_STATE_TABLE"}, TS: time.Unix(0, 200), Val: neighStateTableDeletedJson57},
+            },
+            paths: []string {
+                "NEIGH_STATE_TABLE|10.0.0.57",
+            },
+        },
+        {
+            desc: "Testing deletion of NEIGH_STATE_TABLE:10.0.0.59 and NEIGH_STATE_TABLE 10.0.0.61",
+            q: createStateDbQueryOnChangeMode(t, "NEIGH_STATE_TABLE"),
+            wantNoti: []client.Notification {
+                client.Update{Path: []string{"NEIGH_STATE_TABLE"}, TS: time.Unix(0, 200), Val: neighStateTableJsonTwo},
+                client.Update{Path: []string{"NEIGH_STATE_TABLE"}, TS: time.Unix(0, 200), Val: neighStateTableDeletedJson59},
+                client.Update{Path: []string{"NEIGH_STATE_TABLE"}, TS: time.Unix(0, 200), Val: neighStateTableDeletedJson61},
+            },
+            paths: []string {
+                "NEIGH_STATE_TABLE|10.0.0.59",
+                "NEIGH_STATE_TABLE|10.0.0.61",
+            },
+        },
+    }
+
+    var mutexNoti sync.RWMutex
+    var mutexPaths sync.Mutex
+    for _, tt := range tests {
+        t.Run(tt.desc, func(t *testing.T) {
+            q := tt.q
+            q.Addrs = []string{"127.0.0.1:8081"}
+            c := client.New()
+            defer c.Close()
+            var gotNoti []client.Notification
+            q.NotificationHandler = func(n client.Notification) error {
+                if nn, ok := n.(client.Update); ok {
+                    nn.TS = time.Unix(0, 200)
+                    mutexNoti.Lock()
+                    currentNoti := gotNoti
+                    mutexNoti.Unlock()
+
+                    mutexNoti.RLock()
+                    gotNoti = append(currentNoti, nn)
+                    mutexNoti.RUnlock()
+                }
+                return nil
+            }
+
+            go func() {
+                c.Subscribe(context.Background(), q)
+            }()
+
+            time.Sleep(time.Millisecond * 500) // half a second for subscribe request to sync
+
+            mutexPaths.Lock()
+            paths := tt.paths
+            mutexPaths.Unlock()
+
+            rclient.Del(paths...)
+
+            time.Sleep(time.Millisecond * 1500)
+
+            mutexNoti.Lock()
+            if diff := pretty.Compare(tt.wantNoti, gotNoti); diff != "" {
+                t.Log("\n Want: \n", tt.wantNoti)
+                t.Log("\n Got : \n", gotNoti)
+                t.Errorf("unexpected updates:\n%s", diff)
+            }
+            mutexNoti.Unlock()
+        })
+    }
 }
 
 func TestCPUUtilization(t *testing.T) {

proto/sonic_internal.pb.go
@@ -99,6 +99,7 @@ type Value struct {
 	Fatal string `protobuf:"bytes,6,opt,name=fatal,proto3" json:"fatal,omitempty"`
 	// Notification to be used in place of 1-4 if present
 	Notification *gnmi.Notification `protobuf:"bytes,7,opt,name=notification,proto3" json:"notification,omitempty"`
+	Delete []*gnmi.Path `protobuf:"bytes,8,opt,name=delete,proto3" json:"delete,omitempty"`
 }
 
 func (x *Value) Reset() {
@@ -180,6 +181,13 @@ func (x *Value) GetNotification() *gnmi.Notification {
 		return x.Notification
 	}
 	return nil
+}
+
+func (x *Value) GetDelete() []*gnmi.Path {
+	if x != nil {
+		return x.Delete
+	}
+	return []*gnmi.Path{}
 }
 
 var File_sonic_internal_proto protoreflect.FileDescriptor

sonic_data_client/db_client.go
@@ -379,6 +379,25 @@ func ValToResp(val Value) (*gnmipb.SubscribeResponse, error) {
 				Response: &gnmipb.SubscribeResponse_Update{Update: n}}, nil
 		}
 
+		// In case of path deletion
+		if deleted := val.GetDelete(); deleted != nil {
+			return &gnmipb.SubscribeResponse{
+				Response: &gnmipb.SubscribeResponse_Update{
+					Update: &gnmipb.Notification{
+						Timestamp: val.GetTimestamp(),
+						Prefix:    val.GetPrefix(),
+						Delete:    deleted,
+						Update: []*gnmipb.Update{
+							{
+								Path: val.GetPath(),
+								Val:  val.GetVal(),
+							},
+						},
+					},
+				},
+			}, nil
+		}
+
 		return &gnmipb.SubscribeResponse{
 			Response: &gnmipb.SubscribeResponse_Update{
 				Update: &gnmipb.Notification{
@@ -1061,7 +1080,6 @@ func dbSingleTableKeySubscribe(c *DbClient, rsd redisSubData, updateChannel chan
 			newMsi := make(map[string]interface{})
 			subscr := msgi.(*redis.Message)
 
-			// TODO: support for "Delete []*Path"
 			if subscr.Payload == "del" || subscr.Payload == "hdel" {
 				if tblPath.tableKey != "" {
 					//msi["DEL"] = ""
@@ -1074,6 +1092,7 @@ func dbSingleTableKeySubscribe(c *DbClient, rsd redisSubData, updateChannel chan
 					}
 					key := subscr.Channel[prefixLen:]
 					newMsi[key] = fp
+					newMsi["delete"] = "null_value"
 				}
 			} else if subscr.Payload == "hset" {
 				//op := "SET"
@@ -1145,6 +1164,11 @@ func dbTableKeySubscribe(c *DbClient, gnmiPath *gnmipb.Path, interval time.Durat
 
 	// Helper to send hash data over the stream
 	sendMsiData := func(msiData map[string]interface{}) error {
+		sendDeleteField := false
+		if _, isDelete := msiData["delete"]; isDelete {
+			sendDeleteField = true
+		}
+		delete(msiData, "delete")
 		val, err := msi2TypedValue(msiData)
 		if err != nil {
 			return err
@@ -1156,6 +1180,9 @@ func dbTableKeySubscribe(c *DbClient, gnmiPath *gnmipb.Path, interval time.Durat
 			Path:      gnmiPath,
 			Timestamp: time.Now().UnixNano(),
 			Val:       val,
+		}
+		if sendDeleteField {
+			(*spbv).Delete = []*gnmipb.Path{gnmiPath}
 		}
 		if err = c.q.Put(Value{spbv}); err != nil {
 			return fmt.Errorf("Queue error:  %v", err)

commit b8a24186707b86f8f8fcd9b2f3317332572140e9

Fix random build failures due to sonic_internal.proto
1) Fixed the syntax error in sonic_internal.proto file (introduced by
commit 58a7b20). It appears that sonic_internal.pb.go was manually
updated earlier; hence this syntax error went unnoticed.

2) Removed the makefile target to compile gnmi_server test binary. It
is not used now as 'make check_gotest' runs the go tests directly on
source files. Prevents unnecessary trigger of proto file compilations

Makefile
@@ -15,10 +15,6 @@ export PATH := $(PATH):$(GOBIN):$(shell dirname $(GO))
 export CGO_LDFLAGS := -lswsscommon -lhiredis
 export CGO_CXXFLAGS := -I/usr/include/swss -w -Wall -fpermissive
 
-SRC_FILES=$(shell find . -name '*.go' | grep -v '_test.go' | grep -v '/tests/')
-TEST_FILES=$(wildcard *_test.go)
-TELEMETRY_TEST_DIR = build/tests/gnmi_server
-TELEMETRY_TEST_BIN = $(TELEMETRY_TEST_DIR)/server.test
 ifeq ($(ENABLE_TRANSLIB_WRITE),y)
 BLD_TAGS := gnmi_translib_write
 endif
@@ -39,7 +35,7 @@ GO_DEPS := vendor/.done
 PATCHES := $(wildcard patches/*.patch)
 PATCHES += $(shell find $(MGMT_COMMON_DIR)/patches -type f)
 
-all: sonic-gnmi $(TELEMETRY_TEST_BIN)
+all: sonic-gnmi
 
 go.mod:
 	$(GO) mod init github.com/sonic-net/sonic-gnmi
@@ -139,15 +135,6 @@ endif
 clean:
 	$(RM) -r build
 	$(RM) -r vendor
-
-$(TELEMETRY_TEST_BIN): $(TEST_FILES) $(SRC_FILES)
-	mkdir -p $(@D)
-	cp -r testdata $(@D)/
-ifeq ($(CONFIGURED_ARCH),armhf)
-	touch $@
-else
-	$(GO) test -mod=vendor $(BLD_FLAGS) -c -cover github.com/sonic-net/sonic-gnmi/gnmi_server -o $@
-endif
 
 install:
 	$(INSTALL) -D $(BUILD_DIR)/telemetry $(DESTDIR)/usr/sbin/telemetry

proto/sonic_internal.proto
@@ -5,6 +5,8 @@ import "github.com/openconfig/gnmi/proto/gnmi/gnmi.proto";
 
 package gnmi.sonic;
 
+option go_package = "./;gnmi_sonic";
+
 
 enum State {
   STOPPED = 0;
@@ -36,5 +38,5 @@ message Value {
   gnmi.Notification notification = 7;
 
   // Delete to be used to indicate that node was deleted
-  []gnmi.Path delete = 8;
+  repeated gnmi.Path delete = 8;
 }