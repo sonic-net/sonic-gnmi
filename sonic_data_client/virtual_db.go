d724fbcee2a09818c684f2a167ec1ec0a569f30b
Zain Budhwani <zainbudhwani@microsoft.com>
2023-12-04 23:40:53 +0000

    Replace PFC_WD_TABLE with PFC_WD

--- sonic_data_client/virtual_db.go
+++ sonic_data_client/virtual_db.go
@@ -131,7 +131,7 @@ func getPfcwdMap() (map[string]map[string]string, error) {
 			return nil, err
 		}
 
-		keyName := fmt.Sprintf("PFC_WD_TABLE%v*", separator)
+		keyName := fmt.Sprintf("PFC_WD%v*", separator)
 		resp, err := redisDb.Keys(keyName).Result()
 		if err != nil {
 			log.V(1).Infof("redis get keys failed for %v in namsepace %v, key = %v, err: %v", dbName, namespace, keyName, err)
@@ -145,7 +145,7 @@ func getPfcwdMap() (map[string]map[string]string, error) {
 		}
 
 		for _, key := range resp {
-			name := key[13:]
+			name := key[7:]
 			pfcwdName_map[name] = make(map[string]string)
 		}
 
