89db71e4de2b7d3d3f35223823cae901a16030dc
Zain Budhwani <zainbudhwani@microsoft.com>
2024-02-01 22:35:00 +0000

    Separate main function to support better UT

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -6,237 +6,157 @@ import (
 	"crypto/md5"
 	"flag"
 	"io/ioutil"
-	"strconv"
 	"time"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
+	"sync"
 	log "github.com/golang/glog"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/keepalive"
-
+	"fmt"
 	gnmi "github.com/sonic-net/sonic-gnmi/gnmi_server"
 	testcert "github.com/sonic-net/sonic-gnmi/testdata/tls"
 )
 
-var (
-	userAuth = gnmi.AuthTypes{"password": false, "cert": false, "jwt": false}
-	port = flag.Int("port", -1, "port to listen on")
-	// Certificate files.
-	caCert            = flag.String("ca_crt", "", "CA certificate for client certificate validation. Optional.")
-	serverCert        = flag.String("server_crt", "", "TLS server certificate")
-	serverKey         = flag.String("server_key", "", "TLS server private key")
-	zmqAddress        = flag.String("zmq_address", "", "Orchagent ZMQ address, when not set or empty string telemetry server will switch to Redis based communication channel.")
-	insecure          = flag.Bool("insecure", false, "Skip providing TLS cert and key, for testing only!")
-	noTLS             = flag.Bool("noTLS", false, "disable TLS, for testing only!")
-	allowNoClientCert = flag.Bool("allow_no_client_auth", false, "When set, telemetry server will request but not require a client certificate.")
-	jwtRefInt         = flag.Uint64("jwt_refresh_int", 900, "Seconds before JWT expiry the token can be refreshed.")
-	jwtValInt         = flag.Uint64("jwt_valid_int", 3600, "Seconds that JWT token is valid for.")
-	gnmi_translib_write = flag.Bool("gnmi_translib_write", gnmi.ENABLE_TRANSLIB_WRITE, "Enable gNMI translib write for management framework")
-	gnmi_native_write   = flag.Bool("gnmi_native_write", gnmi.ENABLE_NATIVE_WRITE, "Enable gNMI native write")
-	threshold         = flag.Int("threshold", 100, "max number of client connections")
-	withMasterArbitration = flag.Bool("with-master-arbitration", false, "Enables master arbitration policy.")
-	idle_conn_duration = flag.Int("idle_conn_duration", 5, "Seconds before server closes idle connections")
-)
-
-func main() {
-	flag.Var(userAuth, "client_auth", "Client auth mode(s) - none,cert,password")
-	flag.Parse()
-
-	var defUserAuth gnmi.AuthTypes
-	if *gnmi_translib_write {
-		//In read/write mode we want to enable auth by default.
-		defUserAuth = gnmi.AuthTypes{"password": true, "cert": false, "jwt": true}
-	}else {
-		defUserAuth = gnmi.AuthTypes{"jwt": false, "password": false, "cert": false}
-	}
-
-	if isFlagPassed("client_auth") {
-		log.V(1).Infof("client_auth provided")
-	}else {
-		log.V(1).Infof("client_auth not provided, using defaults.")
-		userAuth = defUserAuth
-	}
-
-	switch {
-	case *port <= 0:
-		log.Errorf("port must be > 0.")
-		return
-	}
-
-	switch {
-	case *threshold < 0:
-		log.Errorf("threshold must be >= 0.")
-		return
-	}
-
-	switch {
-	case *idle_conn_duration < 0:
-		log.Errorf("idle_conn_duration must be >= 0, 0 meaning inf")
-		return
-	}
-
-	gnmi.JwtRefreshInt = time.Duration(*jwtRefInt*uint64(time.Second))
-	gnmi.JwtValidInt = time.Duration(*jwtValInt*uint64(time.Second))
-
-	cfg := &gnmi.Config{}
-	cfg.Port = int64(*port)
-	cfg.EnableTranslibWrite = bool(*gnmi_translib_write)
-	cfg.EnableNativeWrite = bool(*gnmi_native_write)
-	cfg.LogLevel = 3
-	cfg.ZmqAddress = *zmqAddress
-	cfg.Threshold = int(*threshold)
-	cfg.IdleConnDuration = int(*idle_conn_duration)
-	var opts []grpc.ServerOption
-
-	if val, err := strconv.Atoi(getflag("v")); err == nil {
-		cfg.LogLevel = val
-		log.Errorf("flag: log level %v", cfg.LogLevel)
-	}
-
-	if !*noTLS {
-		var certificate tls.Certificate
-		var err error
-		if *insecure {
-			certificate, err = testcert.NewCert()
-			if err != nil {
-				log.Exitf("could not load server key pair: %s", err)
-			}
-		} else {
-			switch {
-			case *serverCert == "":
-				log.Errorf("serverCert must be set.")
-				return
-			case *serverKey == "":
-				log.Errorf("serverKey must be set.")
-				return
-			}
-			certificate, err = tls.LoadX509KeyPair(*serverCert, *serverKey)
-			if err != nil {
-				currentTime := time.Now().UTC()
-				log.Infof("Server Cert md5 checksum: %x at time %s", md5.Sum([]byte(*serverCert)), currentTime.String())
-				log.Infof("Server Key md5 checksum: %x at time %s", md5.Sum([]byte(*serverKey)), currentTime.String())
-				log.Exitf("could not load server key pair: %s", err)
-			}
-		}
-
-		tlsCfg := &tls.Config{
-		ClientAuth:   tls.RequireAndVerifyClientCert,
-		Certificates: []tls.Certificate{certificate},
-		MinVersion:               tls.VersionTLS12,
-		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-		PreferServerCipherSuites: true,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		},
-	}
-
-	if *allowNoClientCert {
-		// RequestClientCert will ask client for a certificate but won't
-		// require it to proceed. If certificate is provided, it will be
-		// verified.
-		tlsCfg.ClientAuth = tls.RequestClientCert
-	}
-
-	if *caCert != "" {
-		ca, err := ioutil.ReadFile(*caCert)
-		if err != nil {
-			log.Exitf("could not read CA certificate: %s", err)
-		}
-		certPool := x509.NewCertPool()
-		if ok := certPool.AppendCertsFromPEM(ca); !ok {
-			log.Exit("failed to append CA certificate")
-		}
-		tlsCfg.ClientCAs = certPool
-	} else {
-		if userAuth.Enabled("cert") {
-			userAuth.Unset("cert")
-			log.Warning("client_auth mode cert requires ca_crt option. Disabling cert mode authentication.")
-		}
-	}
-
-	keep_alive_params := keepalive.ServerParameters{
-		MaxConnectionIdle: time.Duration(cfg.IdleConnDuration) * time.Second, // duration in which idle connection will be closed, default is inf
-	}
-
-	opts = []grpc.ServerOption{grpc.Creds(credentials.NewTLS(tlsCfg))}
-
-	if cfg.IdleConnDuration > 0 { // non inf case
-		opts = append(opts, grpc.KeepaliveParams(keep_alive_params))
-	}
-
-	cfg.UserAuth = userAuth
-
-	gnmi.GenerateJwtSecretKey()
+type TelemetryConfig struct {
+	UserAuth              gnmi.AuthTypes
+	Port                  *int
+	LogLevel              *int
+	CaCert                *string
+	ServerCert            *string
+	ServerKey             *string
+	ZmqAddress            *string
+	Insecure              *bool
+	NoTLS                 *bool
+	AllowNoClientCert     *bool
+	JwtRefInt             *uint64
+	JwtValInt             *uint64
+	GnmiTranslibWrite     *bool
+	GnmiNativeWrite       *bool
+	Threshold             *int
+	WithMasterArbitration *bool
+	IdleConnDuration      *int
 }
 
-	s, err := gnmi.NewServer(cfg, opts)
+func main() {
+	err := runTelemetry(os.Args)
 	if err != nil {
-		log.Errorf("Failed to create gNMI server: %v", err)
-		return
+		log.Errorf("Unable to setup telemetry config due to err: %v", err)
 	}
+}
 
-	if *withMasterArbitration {
-		s.ReqFromMaster = gnmi.ReqFromMasterEnabledMA
+func runTelemetry(args []string) error {
+	fs := flag.NewFlagSet(args[0], flag.ExitOnError)
+	telemetryCfg, cfg, err := setupFlags(fs)
+	if err != nil {
+		return err
 	}
-
-	// This channel is used by goroutine signalhandler to notify when sigterm/sigkill is received so we can stop gnmi server
+	// Reload channel is used as a way for goroutines such as gnmi server Serve to stop when necessary
 	var reload = make(chan int, 1)
-	var wg sync.WaitGroup
-
-	log.V(1).Infof("Auth Modes: %v", userAuth)
-	log.V(1).Infof("Starting RPC server on address: %s", s.Address())
-
-	wg.Add(1)
-
-	go func () {
-		defer wg.Done()
-		if err := s.Serve(); err != nil {
-			log.Errorf("Serve returns with err: %v", err)
-		}
-	}()
-
-	wg.Add(1)
-
 	sigchannel := make(chan os.Signal, 1)
 	signal.Notify(sigchannel,
 		syscall.SIGTERM,
 		syscall.SIGQUIT)
 
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go startGNMIServer(telemetryCfg, cfg, reload, &wg)
+
+	wg.Add(1)
 	go signalHandler(reload, &wg, sigchannel)
 
-	<-reload
-	log.V(1).Infof("Received Notification for gnmi server to shutdown")
-	s.Stop()
-	log.Flush()
 	wg.Wait()
+	return nil
 }
 
-func isFlagPassed(name string) bool {
+func setupFlags(fs *flag.FlagSet) (*TelemetryConfig, *gnmi.Config, error) {
+	telemetryCfg := &TelemetryConfig {
+		UserAuth:              gnmi.AuthTypes{"password": false, "cert": false, "jwt": false},
+		Port:                  fs.Int("port", -1, "port to listen on"),
+		LogLevel:              fs.Int("v", 2, "log level of process"),
+		CaCert:                fs.String("ca_crt", "", "CA certificate for client certificate validation. Optional."),
+		ServerCert:            fs.String("server_crt", "", "TLS server certificate"),
+		ServerKey:             fs.String("server_key", "", "TLS server private key"),
+		ZmqAddress:            fs.String("zmq_address", "", "Orchagent ZMQ address, when not set or empty string telemetry server will switch to Redis based communication channel."),
+		Insecure:              fs.Bool("insecure", false, "Skip providing TLS cert and key, for testing only!"),
+		NoTLS:                 fs.Bool("noTLS", false, "disable TLS, for testing only!"),
+		AllowNoClientCert:     fs.Bool("allow_no_client_auth", false, "When set, telemetry server will request but not require a client certificate."),
+		JwtRefInt:             fs.Uint64("jwt_refresh_int", 900, "Seconds before JWT expiry the token can be refreshed."),
+		JwtValInt:             fs.Uint64("jwt_valid_int", 3600, "Seconds that JWT token is valid for."),
+		GnmiTranslibWrite:     fs.Bool("gnmi_translib_write", gnmi.ENABLE_TRANSLIB_WRITE, "Enable gNMI translib write for management framework"),
+		GnmiNativeWrite:       fs.Bool("gnmi_native_write", gnmi.ENABLE_NATIVE_WRITE, "Enable gNMI native write"),
+		Threshold:             fs.Int("threshold", 100, "max number of client connections"),
+		WithMasterArbitration: fs.Bool("with-master-arbitration", false, "Enables master arbitration policy."),
+		IdleConnDuration:      fs.Int("idle_conn_duration", 5, "Seconds before server closes idle connections"),
+	}
+
+	fs.Var(&telemetryCfg.UserAuth, "client_auth", "Client auth mode(s) - none,cert,password")
+	fs.Parse(os.Args[1:])
+
+	var defUserAuth gnmi.AuthTypes
+	if *telemetryCfg.GnmiTranslibWrite {
+		//In read/write mode we want to enable auth by default.
+		defUserAuth = gnmi.AuthTypes{"password": true, "cert": false, "jwt": true}
+	} else {
+		defUserAuth = gnmi.AuthTypes{"jwt": false, "password": false, "cert": false}
+	}
+
+	if isFlagPassed(fs, "client_auth") {
+		log.V(1).Infof("client_auth provided")
+	} else {
+		log.V(1).Infof("client_auth not provided, using defaults.")
+		telemetryCfg.UserAuth = defUserAuth
+	}
+
+	switch {
+	case *telemetryCfg.Port <= 0:
+		return nil, nil, fmt.Errorf("port must be > 0.")
+	}
+
+	switch {
+	case *telemetryCfg.Threshold < 0:
+		return nil, nil, fmt.Errorf("threshold must be >= 0.")
+	}
+
+	switch {
+	case *telemetryCfg.IdleConnDuration < 0:
+		return nil, nil, fmt.Errorf("idle_conn_duration must be >= 0, 0 meaning inf")
+	}
+
+	switch {
+	case *telemetryCfg.LogLevel < 0:
+		*telemetryCfg.LogLevel = 2
+		log.Infof("Log level must be greater than 0, setting to default value of 2")
+	}
+
+
+	// Move to new function
+	gnmi.JwtRefreshInt = time.Duration(*telemetryCfg.JwtRefInt * uint64(time.Second))
+	gnmi.JwtValidInt = time.Duration(*telemetryCfg.JwtValInt * uint64(time.Second))
+
+	cfg := &gnmi.Config{}
+	cfg.Port = int64(*telemetryCfg.Port)
+	cfg.EnableTranslibWrite = bool(*telemetryCfg.GnmiTranslibWrite)
+	cfg.EnableNativeWrite = bool(*telemetryCfg.GnmiNativeWrite)
+	cfg.LogLevel = int(*telemetryCfg.LogLevel)
+	cfg.ZmqAddress = *telemetryCfg.ZmqAddress
+	cfg.Threshold = int(*telemetryCfg.Threshold)
+	cfg.IdleConnDuration = int(*telemetryCfg.IdleConnDuration)
+
+	return telemetryCfg, cfg, nil
+}
+
+func isFlagPassed(fs *flag.FlagSet, name string) bool {
 	found := false
-	flag.Visit(func(f *flag.Flag) {
+	fs.Visit(func(f *flag.Flag) {
 		if f.Name == name {
 			found = true
 		}
 	})
 	return found
-}
-
-func getflag(name string) string {
-	val := ""
-	flag.VisitAll(func(f *flag.Flag) {
-		if f.Name == name {
-			val = f.Value.String()
-		}
-	})
-	return val
 }
 
 func signalHandler(reload chan<- int, wg *sync.WaitGroup, sigchannel <-chan os.Signal) {
@@ -245,4 +165,114 @@ func signalHandler(reload chan<- int, wg *sync.WaitGroup, sigchannel <-chan os.S
 	case <-sigchannel:
 		reload <- 0
 	}
+}
+
+func startGNMIServer(telemetryCfg *TelemetryConfig, cfg *gnmi.Config, reload <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var opts []grpc.ServerOption
+	if !*telemetryCfg.NoTLS {
+		var certificate tls.Certificate
+		var err error
+		if *telemetryCfg.Insecure {
+			certificate, err = testcert.NewCert()
+			if err != nil {
+				log.Exitf("could not load server key pair: %s", err)
+			}
+		} else {
+			switch {
+			case *telemetryCfg.ServerCert == "":
+				log.Errorf("serverCert must be set.")
+				return
+			case *telemetryCfg.ServerKey == "":
+				log.Errorf("serverKey must be set.")
+				return
+			}
+			certificate, err = tls.LoadX509KeyPair(*telemetryCfg.ServerCert, *telemetryCfg.ServerKey)
+			if err != nil {
+				currentTime := time.Now().UTC()
+				log.Infof("Server Cert md5 checksum: %x at time %s", md5.Sum([]byte(*telemetryCfg.ServerCert)), currentTime.String())
+				log.Infof("Server Key md5 checksum: %x at time %s", md5.Sum([]byte(*telemetryCfg.ServerKey)), currentTime.String())
+				log.Exitf("could not load server key pair: %s", err)
+			}
+		}
+
+		tlsCfg := &tls.Config {
+			ClientAuth:		  tls.RequireAndVerifyClientCert,
+			Certificates:		  []tls.Certificate{certificate},
+			MinVersion:		  tls.VersionTLS12,
+			CurvePreferences:	  []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			PreferServerCipherSuites: true,
+			CipherSuites: []uint16 {
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			},
+		}
+
+		if *telemetryCfg.AllowNoClientCert {
+			// RequestClientCert will ask client for a certificate but won't
+			// require it to proceed. If certificate is provided, it will be
+			// verified.
+			tlsCfg.ClientAuth = tls.RequestClientCert
+		}
+
+		if *telemetryCfg.CaCert != "" {
+			ca, err := ioutil.ReadFile(*telemetryCfg.CaCert)
+			if err != nil {
+				log.Exitf("could not read CA certificate: %s", err)
+			}
+			certPool := x509.NewCertPool()
+			if ok := certPool.AppendCertsFromPEM(ca); !ok {
+				log.Exit("failed to append CA certificate")
+			}
+			tlsCfg.ClientCAs = certPool
+		} else {
+			if telemetryCfg.UserAuth.Enabled("cert") {
+				telemetryCfg.UserAuth.Unset("cert")
+				log.Warning("client_auth mode cert requires ca_crt option. Disabling cert mode authentication.")
+			}
+		}
+
+		keep_alive_params := keepalive.ServerParameters{
+			MaxConnectionIdle: time.Duration(*telemetryCfg.IdleConnDuration) * time.Second, // duration in which idle connection will be closed, default is inf
+		}
+
+		opts = []grpc.ServerOption{grpc.Creds(credentials.NewTLS(tlsCfg))}
+
+		if *telemetryCfg.IdleConnDuration > 0 { // non inf case
+			opts = append(opts, grpc.KeepaliveParams(keep_alive_params))
+		}
+
+		cfg.UserAuth = telemetryCfg.UserAuth
+
+		gnmi.GenerateJwtSecretKey()
+
+	}
+
+	s, err := gnmi.NewServer(cfg, opts)
+	if err != nil {
+		log.Errorf("Failed to create gNMI server: %v", err)
+		return
+	}
+
+	if *telemetryCfg.WithMasterArbitration {
+		s.ReqFromMaster = gnmi.ReqFromMasterEnabledMA
+	}
+
+	log.V(1).Infof("Auth Modes: %v", telemetryCfg.UserAuth)
+	log.V(1).Infof("Starting RPC server on address: %s", s.Address())
+
+	go func() {
+		if err := s.Serve(); err != nil {
+			log.Errorf("Serve returns with err: %v", err)
+		}
+	}()
+
+	<-reload
+	log.V(1).Infof("Received notification for gnmi server to shutdown and rotate certs")
+	s.Stop()
+	log.Flush()
 }
--- telemetry/telemetry_test.go
+++ telemetry/telemetry_test.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-	"os"
-	"testing"
-	"context"
-	"time"
+	"crypto/tls"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"reflect"
 	"sync"
+	"testing"
+	"flag"
+	gnmi "github.com/sonic-net/sonic-gnmi/gnmi_server"
+	"github.com/agiledragon/gomonkey/v2"
+	"os"
 	"syscall"
+	"time"
+	"context"
 )
-
-func TestMain(t *testing.T) {
-}
 
 func TestSignalHandler(t *testing.T) {
 	timeoutInterval := 1
@@ -37,4 +41,147 @@ func TestSignalHandler(t *testing.T) {
 	}
 
 	wg.Wait()
+}
+
+func TestFlags(t *testing.T) {
+	originalArgs := os.Args
+	defer func() {
+		os.Args = originalArgs
+	}()
+
+	tests := []struct {
+		args              []string
+		expectedPort      int
+		expectedThreshold int
+		expectedIdleDur   int
+		expectedLogLevel  int
+	}{
+		{
+			[]string{"cmd", "-port", "9090", "-threshold", "200", "-idle_conn_duration", "10", "-v", "6"},
+			9090,
+			200,
+			10,
+			6,
+		},
+		{
+			[]string{"cmd", "-port", "2020", "-threshold", "500", "-idle_conn_duration", "4", "-v", "0"},
+			2020,
+			500,
+			4,
+			0,
+		},
+		{
+			[]string{"cmd", "-port", "8081", "-threshold", "1", "-idle_conn_duration", "1"},
+			8081,
+			1,
+			1,
+			2,
+		},
+		{
+			[]string{"cmd", "-port", "5050", "-threshold", "10", "-idle_conn_duration", "3", "-v", "-3"},
+			5050,
+			10,
+			3,
+			2,
+		},
+	}
+
+	for _, test := range tests {
+		fs := flag.NewFlagSet("testFlags", flag.ContinueOnError)
+		os.Args = test.args
+
+		config, _, err := setupFlags(fs)
+
+		if err != nil {
+			t.Errorf("Expected err to be nil, got err %v", err)
+		}
+
+		//Verify global var is expected value
+		if *config.Port != test.expectedPort {
+			t.Errorf("Expected port to be %d, got %d", test.expectedPort, *config.Port)
+		}
+
+		if *config.Threshold != test.expectedThreshold {
+			t.Errorf("Expected threshold to be %d, got %d", test.expectedThreshold, *config.Threshold)
+		}
+
+		if *config.IdleConnDuration != test.expectedIdleDur {
+			t.Errorf("Expected idle_conn_duration to be %d, got %d", test.expectedIdleDur, *config.IdleConnDuration)
+		}
+
+		if *config.LogLevel != test.expectedLogLevel {
+			t.Errorf("Expected log_level to be %d, got %d", test.expectedLogLevel, *config.LogLevel)
+		}
+	}
+}
+
+func TestStartGNMIServer(t *testing.T) {
+	testServerCert := "../testdata/testserver.cer"
+	testServerKey := "../testdata/testserver.key"
+	timeoutInterval := 3
+	tick := time.NewTicker(100 * time.Millisecond)
+	defer tick.Stop()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeoutInterval) * time.Second)
+	defer cancel()
+
+	originalArgs := os.Args
+	defer func() {
+		os.Args = originalArgs
+	}()
+
+	fs := flag.NewFlagSet("testStartGNMIServer", flag.ContinueOnError)
+	os.Args = []string{"cmd", "-port", "8080", "-server_crt", testServerCert, "-server_key", testServerKey}
+	telemetryCfg, cfg, err := setupFlags(fs)
+	if err != nil {
+		t.Errorf("Expected err to be nil, got err %v", err)
+	}
+
+	patches := gomonkey.ApplyFunc(tls.LoadX509KeyPair, func(certFile, keyFile string) (tls.Certificate, error) {
+		return tls.Certificate{}, nil
+	})
+	patches.ApplyFunc(gnmi.NewServer, func(cfg *gnmi.Config, opts []grpc.ServerOption) (*gnmi.Server, error) {
+		return &gnmi.Server{}, nil
+	})
+	patches.ApplyFunc(grpc.Creds, func(credentials.TransportCredentials) grpc.ServerOption {
+		return grpc.EmptyServerOption{}
+	})
+	patches.ApplyMethod(reflect.TypeOf(&gnmi.Server{}), "Serve", func(_ *gnmi.Server) error {
+		return nil
+	})
+	patches.ApplyMethod(reflect.TypeOf(&gnmi.Server{}), "Address", func(_ *gnmi.Server) string {
+		return ""
+	})
+
+	reload := make(chan int, 1)
+	wg := &sync.WaitGroup{}
+
+	exitCalled := false
+	patches.ApplyMethod(reflect.TypeOf(&gnmi.Server{}), "Stop", func(_ *gnmi.Server) {
+		exitCalled = true
+	})
+
+	defer patches.Reset()
+
+	wg.Add(1)
+
+	go startGNMIServer(telemetryCfg, cfg, reload, wg)
+
+	select {
+	case <-tick.C: // Simulate shutdown
+		sendShutdownSignal(reload)
+	case <-ctx.Done():
+		t.Errorf("Failed to send shutdown signal")
+		return
+	}
+
+	wg.Wait()
+
+	if !exitCalled {
+		t.Errorf("os.exit should be called if gnmi server is called to shutdown")
+	}
+}
+
+func sendShutdownSignal(reload chan<- int) {
+	reload <- 0
 }
