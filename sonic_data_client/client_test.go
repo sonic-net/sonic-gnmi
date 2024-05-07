5549e5360850597c75ffe4bb0ae343bd3d6f4cda
liuh-80 <liuh@microsoft.com>
2024-05-07 11:44:08 +0000

    Fix test case

--- sonic_data_client/client_test.go
+++ sonic_data_client/client_test.go
@@ -425,7 +425,7 @@ func TestZmqReconnect(t *testing.T) {
 	client := MixedDbClient {
 		applDB : swsscommon.NewDBConnector(APPL_DB_NAME, SWSS_TIMEOUT, false),
 		tableMap : map[string]swsscommon.ProducerStateTable{},
-		zmqTableMap : map[string]swsscommon.ProducerStateTable{},
+		zmqTableMap : map[string]swsscommon.ZmqProducerStateTable{},
 		zmqClient : swsscommon.NewZmqClient(zmqAddress),
 	}
 
