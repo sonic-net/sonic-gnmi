7f39525ebca873d463bf87e4d860ea6ec2a915d8
liuh-80 <liuh@microsoft.com>
2024-05-07 06:02:03 +0000

    Remove json test

--- sonic_data_client/client_test.go
+++ sonic_data_client/client_test.go
@@ -4,7 +4,11 @@ import (
     "sync"
     "errors"
 	"testing"
+	"os"
 	"time"
+	"reflect"
+	"io/ioutil"
+	"encoding/json"
 	"fmt"
 
 	"github.com/jipanyang/gnxi/utils/xpath"
@@ -14,6 +18,297 @@ import (
 )
 
 var testFile string = "/etc/sonic/ut.cp.json"
+
+func JsonEqual(a, b []byte) (bool, error) {
+	var j1, j2 interface{}
+	var err error
+	if err = json.Unmarshal(a, &j1); err != nil {
+		return false, err
+	}
+	if err = json.Unmarshal(b, &j2); err != nil {
+		return false, err
+	}
+	return reflect.DeepEqual(j1, j2), nil
+}
+
+func TestJsonClientNegative(t *testing.T) {
+	os.Remove(testFile)
+	_, err := NewJsonClient(testFile)
+	if err == nil {
+		t.Errorf("Should fail without checkpoint")
+	}
+
+	text := "{"
+	err = ioutil.WriteFile(testFile, []byte(text), 0644)
+	if err != nil {
+		t.Errorf("Fail to create test file")
+	}
+	_, err = NewJsonClient(testFile)
+	if err == nil {
+		t.Errorf("Should fail with invalid checkpoint")
+	}
+}
+
+func TestJsonAdd(t *testing.T) {
+	text := "{}"
+	err := ioutil.WriteFile(testFile, []byte(text), 0644)
+	if err != nil {
+		t.Errorf("Fail to create test file")
+	}
+	client, err := NewJsonClient(testFile)
+	if err != nil {
+		t.Errorf("Create client fail: %v", err)
+	}
+	path_list := [][]string {
+		[]string {
+			"DASH_QOS",
+		},
+		[]string {
+			"DASH_QOS",
+			"qos_02",
+		},
+		[]string {
+			"DASH_QOS",
+			"qos_03",
+			"bw",
+		},
+		[]string {
+			"DASH_VNET",
+			"vnet001",
+			"address_spaces",
+		},
+		[]string {
+			"DASH_VNET",
+			"vnet002",
+			"address_spaces",
+			"0",
+		},
+	}
+	value_list := []string {
+		`{"qos_01": {"bw": "54321", "cps": "1000", "flows": "300"}}`,
+		`{"bw": "10001", "cps": "1001", "flows": "101"}`,
+		`"20001"`,
+		`["10.250.0.0", "192.168.3.0", "139.66.72.9"]`,
+		`"6.6.6.6"`,
+	}
+	for i := 0; i < len(path_list); i++ {
+		path := path_list[i]
+		value := value_list[i]
+		err = client.Add(path, value)
+		if err != nil {
+			t.Errorf("Add %v fail: %v", path, err)
+		}
+		res, err := client.Get(path)
+		if err != nil {
+			t.Errorf("Get %v fail: %v", path, err)
+		}
+		ok, err := JsonEqual([]byte(value), res)
+		if err != nil {
+			t.Errorf("Compare json fail: %v", err)
+			return
+		}
+		if ok != true {
+			t.Errorf("%v and %v do not match", value, string(res))
+		}
+	}
+	path := []string{}
+	res, err := client.Get(path)
+	if err != nil {
+		t.Errorf("Get %v fail: %v", path, err)
+	}
+	t.Logf("Result %s", string(res))
+}
+
+func TestJsonAddNegative(t *testing.T) {
+	text := "{}"
+	err := ioutil.WriteFile(testFile, []byte(text), 0644)
+	if err != nil {
+		t.Errorf("Fail to create test file")
+	}
+	client, err := NewJsonClient(testFile)
+	if err != nil {
+		t.Errorf("Create client fail: %v", err)
+	}
+	path_list := [][]string {
+		[]string {
+			"DASH_QOS",
+		},
+		[]string {
+			"DASH_QOS",
+			"qos_02",
+		},
+		[]string {
+			"DASH_QOS",
+			"qos_03",
+			"bw",
+		},
+		[]string {
+			"DASH_VNET",
+			"vnet001",
+			"address_spaces",
+		},
+		[]string {
+			"DASH_VNET",
+			"vnet002",
+			"address_spaces",
+			"0",
+		},
+		[]string {
+			"DASH_VNET",
+			"vnet002",
+			"address_spaces",
+			"abc",
+		},
+		[]string {
+			"DASH_VNET",
+			"vnet002",
+			"address_spaces",
+			"100",
+		},
+	}
+	value_list := []string {
+		`{"qos_01": {"bw": "54321", "cps": "1000", "flows": "300"}`,
+		`{"bw": "10001", "cps": "1001", "flows": "101"`,
+		`20001`,
+		`["10.250.0.0", "192.168.3.0", "139.66.72.9"`,
+		`"6.6.6.6`,
+		`"6.6.6.6"`,
+		`"6.6.6.6"`,
+	}
+	for i := 0; i < len(path_list); i++ {
+		path := path_list[i]
+		value := value_list[i]
+		err = client.Add(path, value)
+		if err == nil {
+			t.Errorf("Add %v should fail: %v", path, err)
+		}
+	}
+}
+
+func TestJsonRemove(t *testing.T) {
+	text := "{}"
+	err := ioutil.WriteFile(testFile, []byte(text), 0644)
+	if err != nil {
+		t.Errorf("Fail to create test file")
+	}
+	client, err := NewJsonClient(testFile)
+	if err != nil {
+		t.Errorf("Create client fail: %v", err)
+	}
+	path_list := [][]string {
+		[]string {
+			"DASH_QOS",
+		},
+		[]string {
+			"DASH_QOS",
+			"qos_02",
+		},
+		[]string {
+			"DASH_QOS",
+			"qos_03",
+			"bw",
+		},
+		[]string {
+			"DASH_VNET",
+			"vnet001",
+			"address_spaces",
+		},
+		[]string {
+			"DASH_VNET",
+			"vnet002",
+			"address_spaces",
+			"0",
+		},
+	}
+	value_list := []string {
+		`{"qos_01": {"bw": "54321", "cps": "1000", "flows": "300"}}`,
+		`{"bw": "10001", "cps": "1001", "flows": "101"}`,
+		`"20001"`,
+		`["10.250.0.0", "192.168.3.0", "139.66.72.9"]`,
+		`"6.6.6.6"`,
+	}
+	for i := 0; i < len(path_list); i++ {
+		path := path_list[i]
+		value := value_list[i]
+		err = client.Add(path, value)
+		if err != nil {
+			t.Errorf("Add %v fail: %v", path, err)
+		}
+		err = client.Remove(path)
+		if err != nil {
+			t.Errorf("Remove %v fail: %v", path, err)
+		}
+		_, err := client.Get(path)
+		if err == nil {
+			t.Errorf("Get %v should fail: %v", path, err)
+		}
+	}
+}
+
+func TestJsonRemoveNegative(t *testing.T) {
+	text := "{}"
+	err := ioutil.WriteFile(testFile, []byte(text), 0644)
+	if err != nil {
+		t.Errorf("Fail to create test file")
+	}
+	client, err := NewJsonClient(testFile)
+	if err != nil {
+		t.Errorf("Create client fail: %v", err)
+	}
+	path_list := [][]string {
+		[]string {
+			"DASH_QOS",
+		},
+		[]string {
+			"DASH_VNET",
+			"vnet001",
+			"address_spaces",
+		},
+	}
+	value_list := []string {
+		`{"qos_01": {"bw": "54321", "cps": "1000", "flows": "300"}}`,
+		`["10.250.0.0", "192.168.3.0", "139.66.72.9"]`,
+	}
+	for i := 0; i < len(path_list); i++ {
+		path := path_list[i]
+		value := value_list[i]
+		err = client.Add(path, value)
+		if err != nil {
+			t.Errorf("Add %v fail: %v", path, err)
+		}
+	}
+
+	remove_list := [][]string {
+		[]string {
+			"DASH_QOS",
+			"qos_02",
+		},
+		[]string {
+			"DASH_QOS",
+			"qos_03",
+			"bw",
+		},
+		[]string {
+			"DASH_VNET",
+			"vnet001",
+			"address_spaces",
+			"abc",
+		},
+		[]string {
+			"DASH_VNET",
+			"vnet001",
+			"address_spaces",
+			"100",
+		},
+	}
+	for i := 0; i < len(remove_list); i++ {
+		path := remove_list[i]
+		err = client.Remove(path)
+		if err == nil {
+			t.Errorf("Remove %v should fail: %v", path, err)
+		}
+	}
+}
 
 func TestParseDatabase(t *testing.T) {
 	var test_paths []*gnmipb.Path
