c50036dfff4db5f36a551f678d736558e7c53a08
isabelmsft <isabel.li@microsoft.com>
2024-05-23 12:37:42 +0000

    add auth

--- gnmi_server/gnoi.go
+++ gnmi_server/gnoi.go
@@ -42,6 +42,11 @@ func KillOrRestartProcess(restart bool, serviceName string) error {
 }
 
 func (srv *Server) KillProcess(ctx context.Context, req *gnoi_system_pb.KillProcessRequest) (*gnoi_system_pb.KillProcessResponse, error) {
+	_, err := authenticate(srv.config.UserAuth, ctx)
+	if err != nil {
+            return nil, err
+	}
+
 	serviceName := req.GetName()
 	restart := req.GetRestart()
         if req.GetPid() != 0 {
@@ -52,7 +57,7 @@ func (srv *Server) KillProcess(ctx context.Context, req *gnoi_system_pb.KillProc
         }
 	log.V(1).Info("gNOI: KillProcess with optional restart")
 	log.V(1).Info("Request: ", req)
-	err := KillOrRestartProcess(restart, serviceName)
+	err = KillOrRestartProcess(restart, serviceName)
 	if err != nil {
 		return nil, err
 	}
