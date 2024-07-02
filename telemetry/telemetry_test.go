026a56e42bd39b2353f59888c860d2b3e66e0fb8
Zain Budhwani <zainbudhwani@microsoft.com>
2024-07-02 01:00:41 +0000

    Fix UT

--- telemetry/telemetry_test.go
+++ telemetry/telemetry_test.go
@@ -186,7 +186,7 @@ func TestStartGNMIServer(t *testing.T) {
 	wg := &sync.WaitGroup{}
 
 	exitCalled := false
-	patches.ApplyMethod(reflect.TypeOf(&gnmi.Server{}), "Stop", func(_ *gnmi.Server) {
+	patches.ApplyMethod(reflect.TypeOf(&gnmi.Server{}), "ForceStop", func(_ *gnmi.Server) {
 		exitCalled = true
 	})
 
