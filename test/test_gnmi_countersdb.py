a85dfc112da34e2577272484b2ecbbb7f629a3b9
ganglv <88995770+ganglyu@users.noreply.github.com>
2024-07-09 15:39:47 +0800

    Subscribe COUNTERS_DB (#268)
    Why I did it
    gnmi native subscribe API needs to support COUNTERS_DB.
    Microsoft ADO: 25282671
    
    How I did it
    Improve GNMI to support COUNTERS_DB.
    Add unit test to verify subscribe poll mode with COUNTERS_DB.
    
    How to verify it
    Run gnmi unit test.

--- test/test_gnmi_countersdb.py
+++ test/test_gnmi_countersdb.py
@@ -1,7 +1,7 @@
 
 import os
 import time
-from utils import gnmi_set, gnmi_get, gnmi_dump
+from utils import gnmi_get, gnmi_subscribe_poll
 
 import pytest
 
@@ -19,3 +19,31 @@ class TestGNMICountersDb:
 
         ret, msg_list = gnmi_get(get_list)
         assert ret == 0, 'Fail to read COUNTERS table, ' + msg_list[0]
+
+    def test_gnmi_get_table_02(self):
+        get_list = ['/sonic-db:COUNTERS_DB/localhost/COUNTERS/oid:0x1000000000003']
+
+        ret, msg_list = gnmi_get(get_list)
+        assert ret == 0, 'Fail to read COUNTERS table, ' + msg_list[0]
+
+    def test_gnmi_get_table_03(self):
+        get_list = ['/sonic-db:COUNTERS_DB/localhost/COUNTERS_PORT_NAME_MAP/Ethernet10']
+
+        ret, msg_list = gnmi_get(get_list)
+        assert ret == 0, 'Fail to read COUNTERS table, ' + msg_list[0]
+
+    def test_gnmi_poll_table_01(self):
+        path = "/COUNTERS_DB/localhost/COUNTERS_PORT_NAME_MAP/Ethernet10"
+        cnt = 3
+        interval = 1
+        ret, msg = gnmi_subscribe_poll(path, interval, cnt, timeout=0)
+        assert ret == 0, 'Fail to subscribe: ' + msg
+        assert msg.count("COUNTERS_PORT_NAME_MAP") == cnt, 'Invalid result: ' + msg
+
+    def test_gnmi_poll_table_02(self):
+        path = "/COUNTERS_DB/localhost/COUNTERS/oid:0x1000000000003"
+        cnt = 3
+        interval = 1
+        ret, msg = gnmi_subscribe_poll(path, interval, cnt, timeout=0)
+        assert ret == 0, 'Fail to subscribe: ' + msg
+        assert msg.count("oid:0x1000000000003") == cnt, 'Invalid result: ' + msg
