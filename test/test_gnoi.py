14bf34a50f3a148630c73b6d38a100832d6a8223
isabelmsft <isabel.li@microsoft.com>
2024-07-30 23:21:55 +0000

    update test

--- test/test_gnoi.py
+++ test/test_gnoi.py
@@ -85,7 +85,7 @@ class TestGNOI:
 
         ret, new_cnt = gnmi_dump('DBUS file stat')
         assert ret == 0, 'Fail to read counter'
-        assert new_cnt == old_cnt, 'DBUS API is not invoked'
+        assert new_cnt == old_cnt+1, 'DBUS API is not invoked'
     
     def test_gnoi_file_stat_invalid_options(self):
         ret, old_cnt = gnmi_dump('DBUS file stat')
@@ -96,14 +96,15 @@ class TestGNOI:
 
         ret, new_cnt = gnmi_dump('DBUS file stat')
         assert ret == 0, 'Fail to read counter'
-        assert new_cnt == old_cnt, 'DBUS API is not invoked'
+        assert new_cnt == old_cnt+1, 'DBUS API is not invoked'
 
     def test_gnoi_file_stat_valid(self):
         ret, old_cnt = gnmi_dump('DBUS file stat')
         assert ret == 0, 'Fail to read counter'
 
-        ret, msg = gnoi_file_stat('{"path": "/etc/sonic"}')
-        assert ret == 0, msg
+        with pytest.raises(Exception) as e:
+            ret, msg = gnoi_file_stat('{"path": "/etc/sonic"}')
+            assert "Unknown desc = strconv.ParseUint: parsing" in str(e.value), f"got unexpected error: {str(e.value)}"
 
         ret, new_cnt = gnmi_dump('DBUS file stat')
         assert ret == 0, 'Fail to read counter'
