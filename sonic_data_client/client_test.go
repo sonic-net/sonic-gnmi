209f81882421524bf0d48275dd6d7d75bd2dad72
Mihut Aronovici <aronovic@cisco.com>
2024-10-07 10:03:42 -0400

    modify the test code to use ips@ instead of ips for key
    Signed-off-by: Mihut Aronovici <aronovic@cisco.com>

--- sonic_data_client/client_test.go
+++ sonic_data_client/client_test.go
@@ -733,7 +733,7 @@ func TestGetDpuAddress(t *testing.T) {
 		t.Errorf("get DPU address should failed: %v, but get %s", err, address)
 	}
 
-	dhcpPortTable.Hset("bridge_midplane|dpu0", "ips", "127.0.0.2,127.0.0.1")
+	dhcpPortTable.Hset("bridge_midplane|dpu0", "ips@", "127.0.0.2,127.0.0.1")
 
 	// test get valid DPU address
 	address, err = getDpuAddress("dpu0")
