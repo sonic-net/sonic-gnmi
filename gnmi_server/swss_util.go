2a04d63acdf01317d1b52efdaa45b08900ff476b
Neha Das <neha.das43@gmail.com>
2025-01-30 16:15:15 -0800

    Update swss_util.go

--- gnmi_server/swss_util.go
+++ gnmi_server/swss_util.go
@@ -34,3 +34,34 @@ func SwssToErrorCode(statusStr string) codes.Code {
 	}
 	return codes.Internal
 }
+
+// Converts gRPC Code to a SWSS error code string.
+func ErrorCodeToSwss(errCode codes.Code) string {
+	switch errCode {
+	case codes.OK:
+		return "SWSS_RC_SUCCESS"
+	case codes.Unknown:
+		return "SWSS_RC_UNKNOWN"
+	case codes.InvalidArgument:
+		return "SWSS_RC_INVALID_PARAM"
+	case codes.DeadlineExceeded:
+		return "SWSS_RC_DEADLINE_EXCEEDED"
+	case codes.NotFound:
+		return "SWSS_RC_NOT_FOUND"
+	case codes.AlreadyExists:
+		return "SWSS_RC_EXISTS"
+	case codes.PermissionDenied:
+		return "SWSS_RC_PERMISSION_DENIED"
+	case codes.ResourceExhausted:
+		return "SWSS_RC_FULL"
+	case codes.Unimplemented:
+		return "SWSS_RC_UNIMPLEMENTED"
+	case codes.Internal:
+		return "SWSS_RC_INTERNAL"
+	case codes.FailedPrecondition:
+		return "SWSS_RC_FAILED_PRECONDITION"
+	case codes.Unavailable:
+		return "SWSS_RC_UNAVAIL"
+	}
+	return ""
+}
