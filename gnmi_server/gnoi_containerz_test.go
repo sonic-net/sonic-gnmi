5736a3c542d68e47502557fcf56f1bf64afa585f
hdwhdw <dwhuang9@gmail.com>
2025-04-18 16:31:15 +0000

    add unit test.

---
+++ gnmi_server/gnoi_containerz_test.go
@@ -0,0 +1,66 @@
+package gnmi
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	gnoi_containerz_pb "github.com/openconfig/gnoi/containerz"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type dummyDeployServer struct {
+    gnoi_containerz_pb.Containerz_DeployServer
+}
+
+type dummyListServer struct {
+    gnoi_containerz_pb.Containerz_ListServer
+}
+
+type dummyLogServer struct {
+    gnoi_containerz_pb.Containerz_LogServer
+}
+
+func TestContainerzServer_Unimplemented(t *testing.T) {
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
+
+    server := &ContainerzServer{}
+
+    // Test Deploy
+    err := server.Deploy(&dummyDeployServer{})
+    if err == nil || status.Code(err) != codes.Unimplemented {
+        t.Errorf("Deploy: expected Unimplemented error, got %v", err)
+    }
+
+    // Test Remove
+    _, err = server.Remove(context.Background(), &gnoi_containerz_pb.RemoveRequest{})
+    if err == nil || status.Code(err) != codes.Unimplemented {
+        t.Errorf("Remove: expected Unimplemented error, got %v", err)
+    }
+
+    // Test List
+    err = server.List(&gnoi_containerz_pb.ListRequest{}, &dummyListServer{})
+    if err == nil || status.Code(err) != codes.Unimplemented {
+        t.Errorf("List: expected Unimplemented error, got %v", err)
+    }
+
+    // Test Start
+    _, err = server.Start(context.Background(), &gnoi_containerz_pb.StartRequest{})
+    if err == nil || status.Code(err) != codes.Unimplemented {
+        t.Errorf("Start: expected Unimplemented error, got %v", err)
+    }
+
+    // Test Stop
+    _, err = server.Stop(context.Background(), &gnoi_containerz_pb.StopRequest{})
+    if err == nil || status.Code(err) != codes.Unimplemented {
+        t.Errorf("Stop: expected Unimplemented error, got %v", err)
+    }
+
+    // Test Log
+    err = server.Log(&gnoi_containerz_pb.LogRequest{}, &dummyLogServer{})
+    if err == nil || status.Code(err) != codes.Unimplemented {
+        t.Errorf("Log: expected Unimplemented error, got %v", err)
+    }
+}
