3e9d2a31c0d63cecbcd02231eedbcbb8afa01d00
hdwhdw <dwhuang9@gmail.com>
2025-04-21 18:45:27 +0000

    various work

--- gnmi_server/gnoi_containerz.go
+++ gnmi_server/gnoi_containerz.go
@@ -5,6 +5,7 @@ package gnmi
 import (
 	"context"
 
+	log "github.com/golang/glog"
 	gnoi_containerz_pb "github.com/openconfig/gnoi/containerz"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -12,30 +13,36 @@ import (
 
 // Deploy is a placeholder implementation for the Deploy RPC.
 func (c *ContainerzServer) Deploy(stream gnoi_containerz_pb.Containerz_DeployServer) error {
+	log.V(2).Info("gNOI: Containerz Deploy called")
 	return status.Error(codes.Unimplemented, "Deploy is not implemented")
 }
 
 // Remove is a placeholder implementation for the Remove RPC.
 func (c *ContainerzServer) Remove(ctx context.Context, req *gnoi_containerz_pb.RemoveRequest) (*gnoi_containerz_pb.RemoveResponse, error) {
+	log.V(2).Info("gNOI: Containerz Remove called")
 	return nil, status.Error(codes.Unimplemented, "Remove is not implemented")
 }
 
 // List is a placeholder implementation for the List RPC.
 func (c *ContainerzServer) List(req *gnoi_containerz_pb.ListRequest, stream gnoi_containerz_pb.Containerz_ListServer) error {
+	log.V(2).Info("gNOI: Containerz List called")
 	return status.Error(codes.Unimplemented, "List is not implemented")
 }
 
 // Start is a placeholder implementation for the Start RPC.
 func (c *ContainerzServer) Start(ctx context.Context, req *gnoi_containerz_pb.StartRequest) (*gnoi_containerz_pb.StartResponse, error) {
+	log.V(2).Info("gNOI: Containerz Start called")
 	return nil, status.Error(codes.Unimplemented, "Start is not implemented")
 }
 
 // Stop is a placeholder implementation for the Stop RPC.
 func (c *ContainerzServer) Stop(ctx context.Context, req *gnoi_containerz_pb.StopRequest) (*gnoi_containerz_pb.StopResponse, error) {
+	log.V(2).Info("gNOI: Containerz Stop called")
 	return nil, status.Error(codes.Unimplemented, "Stop is not implemented")
 }
 
 // Log is a placeholder implementation for the Log RPC.
 func (c *ContainerzServer) Log(req *gnoi_containerz_pb.LogRequest, stream gnoi_containerz_pb.Containerz_LogServer) error {
+	log.V(2).Info("gNOI: Containerz Log called")
 	return status.Error(codes.Unimplemented, "Log is not implemented")
 }
