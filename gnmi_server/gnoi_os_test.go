25f76e7db24f9d55bcaee1fd35a8860f454cfd67
Bibhuprasad Singh <bibhuprasad@google.com>
2025-06-06 12:01:03 +0000

    Added dbus_client to gnoi Install RPC

--- gnmi_server/gnoi_os_test.go
+++ gnmi_server/gnoi_os_test.go
@@ -3,35 +3,61 @@ package gnmi
 import (
 	"context"
 	"crypto/tls"
-	"io/ioutil"
+	"fmt"
 	"os"
 	"testing"
 	"time"
 
+	log "github.com/golang/glog"
 	ospb "github.com/openconfig/gnoi/os"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
+	json "google.golang.org/protobuf/encoding/protojson"
 )
+
+// ProcessFakeTrfReady responds with the TransferReady response.
+func ProcessFakeTrfReady(req string) (string, error) {
+	// Fake response.
+	resp := &ospb.InstallResponse{
+		Response: &ospb.InstallResponse_TransferReady{},
+	}
+
+	respStr, err := json.Marshal(resp)
+	if err != nil {
+		log.Errorln("Cannot marshal TransferReady response!")
+		return "", fmt.Errorf("Cannot marshal TransferReady response!")
+	}
+
+	return string(respStr), nil
+}
+
+// ProcessFakeTrfEnd responds with the Validated response.
+func ProcessFakeTrfEnd(req string) (string, error) {
+	// Fake response.
+	resp := &ospb.InstallResponse{
+		Response: &ospb.InstallResponse_Validated{},
+	}
+	respStr, err := json.Marshal(resp)
+	if err != nil {
+		log.Errorln("Cannot marshal TransferEnd response!")
+		return "", fmt.Errorf("Cannot marshal TransferEnd response!")
+	}
+	return string(respStr), nil
+}
+
+//func clearAlarm(t *testing.T) {
+//	rc := getRedisClient(t, "STATE_DB")
+//	defer rc.Close()
+//	if err := rc.Del(context.Background(), "COMPONENT_STATE_TABLE|Telemetry").Err(); err != nil {
+//		t.Fatalf("Failed to clear component state information in DB: %v\n", err)
+//	}
+//}
 
 var testOSCases = []struct {
 	desc string
 	f    func(ctx context.Context, t *testing.T, sc ospb.OSClient, s *OSServer)
 }{
-	{
-		desc: "OSActivateFailsAsBackEndIsUnimplemented",
-		f: func(ctx context.Context, t *testing.T, sc ospb.OSClient, s *OSServer) {
-			_, err := sc.Activate(ctx, &ospb.ActivateRequest{})
-			testErr(err, codes.Internal, "Internal SONiC HostService failure", t)
-		},
-	},
-	{
-		desc: "OSVerifyFailsAsBackEndIsUnimplemented",
-		f: func(ctx context.Context, t *testing.T, sc ospb.OSClient, s *OSServer) {
-			_, err := sc.Verify(ctx, &ospb.VerifyRequest{})
-			testErr(err, codes.Internal, "Internal SONiC HostService failure", t)
-		},
-	},
 	{
 		desc: "OSInstallFailsIfTransferRequestIsMissingVersion",
 		f: func(ctx context.Context, t *testing.T, sc ospb.OSClient, s *OSServer) {
@@ -60,6 +86,7 @@ var testOSCases = []struct {
 			if instErr.GetType() != ospb.InstallError_PARSE_FAIL {
 				t.Fatal("Expected InstallError type: PARSE_FAIL!")
 			}
+
 			// Receive error reporting.
 			_, err = stream.Recv()
 			if err == nil {
@@ -96,22 +123,19 @@ var testOSCases = []struct {
 			if resp.GetTransferReady() == nil {
 				t.Fatal("Did not receive expected TransferReady response")
 			}
+			targetAddr := fmt.Sprintf("127.0.0.1:%d", s.config.Port)
 
 			// Create a new client.
 			tlsConfig := &tls.Config{InsecureSkipVerify: true}
 			opts := []grpc.DialOption{grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig))}
-			targetAddr := "127.0.0.1:8081"
 			conn, err := grpc.Dial(targetAddr, opts...)
 			if err != nil {
 				t.Fatalf("Dialing to %s failed: %v", targetAddr, err)
 			}
 			defer conn.Close()
-
 			newsc := ospb.NewOSClient(conn)
-
 			newctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 			defer cancel()
-
 			newstream, err := newsc.Install(newctx, grpc.EmptyCallOption{})
 			if err != nil {
 				t.Fatal(err.Error())
@@ -234,6 +258,7 @@ var testOSCases = []struct {
 			if err := f.Close(); err != nil {
 				t.Fatal(err.Error())
 			}
+
 			// Cleanup
 			defer func() {
 				if err := os.Remove(imgPath); err != nil {
@@ -241,17 +266,10 @@ var testOSCases = []struct {
 				}
 			}()
 
-			// Get some file to transfer. Repurpose cert data.
-			fileName := "../testdata/TEST_DATA.txt"
-			data, err := ioutil.ReadFile(fileName)
-			if err != nil {
-				t.Fatal(err.Error())
-			}
-
 			// Send TransferContent.
 			err = stream.Send(&ospb.InstallRequest{
 				Request: &ospb.InstallRequest_TransferContent{
-					TransferContent: data,
+					TransferContent: []byte("unimportant string"),
 				},
 			})
 			if err != nil {
@@ -276,14 +294,14 @@ var testOSCases = []struct {
 		},
 	},
 	{
-		desc: "OSInstallSucceeds",
+		desc: "OSInstallFailsIfStreamClosesInTheMiddleOfTransfer",
 		f: func(ctx context.Context, t *testing.T, sc ospb.OSClient, s *OSServer) {
 			stream, err := sc.Install(ctx, grpc.EmptyCallOption{})
 			if err != nil {
 				t.Fatal(err.Error())
 			}
-
 			version := "os1.1"
+
 			// Send TransferRequest.
 			err = stream.Send(&ospb.InstallRequest{
 				Request: &ospb.InstallRequest_TransferRequest{
@@ -305,12 +323,144 @@ var testOSCases = []struct {
 				t.Fatal("Did not receive expected TransferReady response")
 			}
 
-			// Get some file to transfer. Repurpose cert data.
-			fileName := "../testdata/TEST_DATA.txt"
-			data, err := ioutil.ReadFile(fileName)
+			// Send TransferContent.
+			err = stream.Send(&ospb.InstallRequest{
+				Request: &ospb.InstallRequest_TransferContent{
+					TransferContent: []byte("unimportant string"),
+				},
+			})
 			if err != nil {
 				t.Fatal(err.Error())
 			}
+
+			// Receive TransferProgress response.
+			resp, err = stream.Recv()
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+			if trfProg := resp.GetTransferProgress(); trfProg == nil {
+				t.Fatal("Did not receive expected TransferProgress response")
+			}
+
+			// Close the stream immediately.
+			stream.CloseSend()
+
+			// Receive error reporting premature closure of the stream.
+			_, err = stream.Recv()
+			if err == nil {
+				t.Fatal("Expected an error reporting on premature closure of the stream.")
+			}
+
+			// Check incomplete transfer is removed!
+			if s.imageExists(s.getVersionPath(version)) {
+				t.Fatal("Incomplete image should have been deleted!")
+			}
+		},
+	},
+	{
+		desc: "OSInstallFailsIfWrongMsgIsSentInTheMiddleOfTransfer",
+		f: func(ctx context.Context, t *testing.T, sc ospb.OSClient, s *OSServer) {
+			stream, err := sc.Install(ctx, grpc.EmptyCallOption{})
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+			version := "os1.1"
+
+			// Send TransferRequest.
+			err = stream.Send(&ospb.InstallRequest{
+				Request: &ospb.InstallRequest_TransferRequest{
+					TransferRequest: &ospb.TransferRequest{
+						Version: version,
+					},
+				},
+			})
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			// Receive TransferReady response.
+			resp, err := stream.Recv()
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+			if trfReady := resp.GetTransferReady(); trfReady == nil {
+				t.Fatal("Did not receive expected TransferReady response")
+			}
+
+			// Send TransferContent.
+			err = stream.Send(&ospb.InstallRequest{
+				Request: &ospb.InstallRequest_TransferContent{
+					TransferContent: []byte("unimportant string"),
+				},
+			})
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			// Receive TransferProgress response.
+			resp, err = stream.Recv()
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+			if trfProg := resp.GetTransferProgress(); trfProg == nil {
+				t.Fatal("Did not receive expected TransferProgress response")
+			}
+
+			// Send TransferRequest again. This is unexpected!
+			// Server should send error message, clean up incomplete transfer.
+			err = stream.Send(&ospb.InstallRequest{
+				Request: &ospb.InstallRequest_TransferRequest{
+					TransferRequest: &ospb.TransferRequest{
+						Version: version,
+					},
+				},
+			})
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			// Receive error reporting.
+			_, err = stream.Recv()
+			if err == nil {
+				t.Fatal("Expected an error reporting on premature closure of the stream.")
+			}
+
+			// Check incomplete transfer is removed!
+			if s.imageExists(s.getVersionPath(version)) {
+				t.Fatal("Incomplete image should have been deleted!")
+			}
+		},
+	},
+	{
+		desc: "OSInstallSucceeds",
+		f: func(ctx context.Context, t *testing.T, sc ospb.OSClient, s *OSServer) {
+			stream, err := sc.Install(ctx, grpc.EmptyCallOption{})
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+			version := "os1.1"
+
+			// Send TransferRequest.
+			err = stream.Send(&ospb.InstallRequest{
+				Request: &ospb.InstallRequest_TransferRequest{
+					TransferRequest: &ospb.TransferRequest{
+						Version: version,
+					},
+				},
+			})
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			// Receive TransferReady response.
+			resp, err := stream.Recv()
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+			if trfReady := resp.GetTransferReady(); trfReady == nil {
+				t.Fatal("Did not receive expected TransferReady response")
+			}
+			data := []byte("unimportant string")
 
 			// Send TransferContent.
 			err = stream.Send(&ospb.InstallRequest{
@@ -350,7 +500,7 @@ var testOSCases = []struct {
 
 			// Sanity check!
 			imgPath := s.getVersionPath(version)
-			dataRead, err := ioutil.ReadFile(imgPath)
+			dataRead, err := os.ReadFile(imgPath)
 			if err != nil {
 				t.Fatal(err.Error())
 			}
@@ -370,20 +520,17 @@ var testOSCases = []struct {
 func TestOSServer(t *testing.T) {
 	s := createServer(t, 8081)
 	go runServer(t, s)
-	defer s.Stop()
-
+	defer s.s.Stop()
+	targetAddr := fmt.Sprintf("127.0.0.1:%d", s.config.Port)
 	tlsConfig := &tls.Config{InsecureSkipVerify: true}
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig))}
-	targetAddr := "127.0.0.1:8081"
 	conn, err := grpc.Dial(targetAddr, opts...)
 	if err != nil {
 		t.Fatalf("Dialing to %s failed: %v", targetAddr, err)
 	}
 	defer conn.Close()
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
 	for _, test := range testOSCases {
 		t.Run(test.desc, func(t *testing.T) {
 			conn, err := grpc.Dial(targetAddr, opts...)
@@ -391,7 +538,6 @@ func TestOSServer(t *testing.T) {
 				t.Fatalf("Dialing to %s failed: %v", targetAddr, err)
 			}
 			defer conn.Close()
-
 			sc := ospb.NewOSClient(conn)
 			test.f(ctx, t, sc, &OSServer{Server: s})
 		})
