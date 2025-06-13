3c5a7d6a135d7294dfe098aa1f51f86879d5655f
Zain Budhwani <99770260+zbud-msft@users.noreply.github.com>
2025-06-13 15:57:02 -0700

    [202411] Manual backport of Fix poll mode for querying non existing table and key for APPL_DB (#419)
    Why I did it
    Backport of #375
    
    MS: 32418202
    
    SONiC telemetry does not behave according to gnmi specifications
    
    As per gnmi specification regarding missing data, "There is no requirement that the path specified in the message must exist within the current data tree on the server. While the path within the subscription SHOULD be a valid path within the set of schema modules that the target supports, subscribing to any syntactically valid path within such modules MUST be allowed. In the case that a particular path does not (yet) exist, the target MUST NOT close the RPC and instead should continue to monitor for the existence of the path, and transmit telemetry updates should it exist in the future."
    
    As per gnmi specification regarding deleted keys/paths, "Where a node within the subscribed paths has been removed, the delete field of the Notification message MUST have the path of the node that has been removed appended to it."
    
    How I did it
    If there is missing data, we will not return any failures, but instead we will send sync responses only until data is on the device for polling APPL_DB
    
    If data is deleted, we will send a delete notification to tell client that path they are querying is deleted and after send sync responses for polling APPL_DB.
    
    How to verify it
    UT and reverified on 20241110.17

--- gnmi_server/client_subscribe.go
+++ gnmi_server/client_subscribe.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"strings"
 	"sync"
 
 	"github.com/Workiva/go-datastructures/queue"
@@ -59,8 +60,8 @@ func (c *Client) setConnectionManager(threshold int) {
 		return
 	}
 	connectionManager = &ConnectionManager {
-		connections: make(map[string]struct{}),
-		threshold:   threshold,
+			connections: make(map[string]struct{}),
+			threshold:   threshold,
 	}
 	connectionManager.PrepareRedis()
 }
@@ -195,7 +196,11 @@ func (c *Client) Run(stream gnmipb.GNMI_SubscribeServer) (err error) {
 		c.polled = make(chan struct{}, 1)
 		c.polled <- struct{}{}
 		c.w.Add(1)
-		go dc.PollRun(c.q, c.polled, &c.w, c.subscribe)
+		if target == "APPL_DB" || strings.HasPrefix(target, "APPL_DB/") {
+			go dc.AppDBPollRun(c.q, c.polled, &c.w, c.subscribe)
+		} else {
+			go dc.PollRun(c.q, c.polled, &c.w, c.subscribe)
+		}
 	case gnmipb.SubscriptionList_ONCE:
 		c.once = make(chan struct{}, 1)
 		c.once <- struct{}{}
