3548281a31a03f27be53518e714300ace486b34d
Kanchana <velusamyk@google.com>
2025-07-17 05:58:18 +0530

    Add support for gNOI File Services (#406)

--- gnmi_server/gnoi.go
+++ gnmi_server/gnoi.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	jwt "github.com/dgrijalva/jwt-go"
 	log "github.com/golang/glog"
-	gnoi_file_pb "github.com/openconfig/gnoi/file"
 	gnoi_os_pb "github.com/openconfig/gnoi/os"
 	spb "github.com/sonic-net/sonic-gnmi/proto/gnoi"
 	spb_jwt "github.com/sonic-net/sonic-gnmi/proto/gnoi/jwt"
@@ -15,7 +14,6 @@ import (
 	"google.golang.org/grpc/status"
 	"os"
 	"os/user"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -23,78 +21,6 @@ import (
 const (
 	stateDB string = "STATE_DB"
 )
-
-func ReadFileStat(path string) (*gnoi_file_pb.StatInfo, error) {
-	sc, err := ssc.NewDbusClient()
-	if err != nil {
-		return nil, err
-	}
-	defer sc.Close()
-
-	log.V(2).Infof("Reading file stat at path %s...", path)
-	data, err := sc.GetFileStat(path)
-	if err != nil {
-		log.V(2).Infof("Failed to read file stat at path %s: %v. Error ", path, err)
-		return nil, err
-	}
-	// Parse the data and populate StatInfo
-	lastModified, err := strconv.ParseUint(data["last_modified"], 10, 64)
-	if err != nil {
-		return nil, err
-	}
-
-	permissions, err := strconv.ParseUint(data["permissions"], 8, 32)
-	if err != nil {
-		return nil, err
-	}
-
-	size, err := strconv.ParseUint(data["size"], 10, 64)
-	if err != nil {
-		return nil, err
-	}
-
-	umaskStr := data["umask"]
-	if strings.HasPrefix(umaskStr, "o") {
-		umaskStr = umaskStr[1:] // Remove leading "o"
-	}
-	umask, err := strconv.ParseUint(umaskStr, 8, 32)
-	if err != nil {
-		return nil, err
-	}
-
-	statInfo := &gnoi_file_pb.StatInfo{
-		Path:         data["path"],
-		LastModified: lastModified,
-		Permissions:  uint32(permissions),
-		Size:         size,
-		Umask:        uint32(umask),
-	}
-	return statInfo, nil
-}
-
-func (srv *FileServer) Stat(ctx context.Context, req *gnoi_file_pb.StatRequest) (*gnoi_file_pb.StatResponse, error) {
-	_, err := authenticate(srv.config, ctx, "gnoi", false)
-	if err != nil {
-		return nil, err
-	}
-	path := req.GetPath()
-	log.V(1).Info("gNOI: Read File Stat")
-	log.V(1).Info("Request: ", req)
-	statInfo, err := ReadFileStat(path)
-	if err != nil {
-		return nil, err
-	}
-	resp := &gnoi_file_pb.StatResponse{
-		Stats: []*gnoi_file_pb.StatInfo{statInfo},
-	}
-	return resp, nil
-}
-
-// TODO: Support GNOI File Get
-func (srv *FileServer) Get(req *gnoi_file_pb.GetRequest, stream gnoi_file_pb.File_GetServer) error {
-	log.V(1).Info("gNOI: File Get")
-	return status.Errorf(codes.Unimplemented, "")
-}
 
 func (srv *OSServer) Verify(ctx context.Context, req *gnoi_os_pb.VerifyRequest) (*gnoi_os_pb.VerifyResponse, error) {
 	_, err := authenticate(srv.config, ctx, "gnoi", false)
