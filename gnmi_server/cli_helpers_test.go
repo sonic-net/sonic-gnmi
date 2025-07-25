580595c98ecb479b8ce2139c61fc4c328bf571c4
Zain Budhwani <99770260+zbud-msft@users.noreply.github.com>
2025-07-25 14:19:41 -0700

    Add SHOW implementation for ipv6 bgp summary (#458)

---
+++ gnmi_server/cli_helpers_test.go
@@ -0,0 +1,68 @@
+package gnmi
+
+import (
+	sdcfg "github.com/sonic-net/sonic-gnmi/sonic_db_config"
+	"io/ioutil"
+	"os/exec"
+	"reflect"
+	"testing"
+
+	"github.com/agiledragon/gomonkey/v2"
+	sdc "github.com/sonic-net/sonic-gnmi/sonic_data_client"
+)
+
+const (
+	ServerPort   = 8081
+	StateDbNum   = 6
+	ConfigDbNum  = 4
+	TargetAddr   = "127.0.0.1:8081"
+	QueryTimeout = 10
+)
+
+func MockNSEnterBGPSummary(t *testing.T, fileName string) *gomonkey.Patches {
+	fileContentBytes, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		t.Fatalf("read file %v err: %v", fileName, err)
+	}
+	patches := gomonkey.ApplyFunc(exec.Command, func(name string, args ...string) *exec.Cmd {
+		return &exec.Cmd{}
+	})
+	patches.ApplyMethod(reflect.TypeOf(&exec.Cmd{}), "CombinedOutput", func(_ *exec.Cmd) ([]byte, error) {
+		return fileContentBytes, nil
+	})
+	return patches
+}
+
+func MockReadFile(fileName string, fileContent string, fileReadErr error) {
+	sdc.ImplIoutilReadFile = func(filePath string) ([]byte, error) {
+		if filePath == fileName {
+			if fileReadErr != nil {
+				return nil, fileReadErr
+			}
+			return []byte(fileContent), nil
+		}
+		return ioutil.ReadFile(filePath)
+	}
+}
+
+func FlushDataSet(t *testing.T, dbNum int) {
+	ns, _ := sdcfg.GetDbDefaultNamespace()
+	rclient := getRedisClientN(t, dbNum, ns)
+	defer rclient.Close()
+	rclient.FlushDB()
+}
+
+func AddDataSet(t *testing.T, dbNum int, fileName string) {
+	ns, _ := sdcfg.GetDbDefaultNamespace()
+	rclient := getRedisClientN(t, dbNum, ns)
+	defer rclient.Close()
+	rclient.FlushDB()
+
+	fileContentBytes, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		t.Fatalf("read file: %v, err: %v", fileName, err)
+	}
+
+	fileContent := loadConfig(t, "", fileContentBytes)
+	loadDB(t, rclient, fileContent)
+}
