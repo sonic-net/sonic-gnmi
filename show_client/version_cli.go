e6ea624238e64dc30569a2f3cd3dd71ad3bba8a5
sharmavijay@microsoft.com <root@sharmavijay-dev-vm.d4y3nv5wwgfelhhopdxv1tqjld.dx.internal.cloudapp.net>
2025-07-28 10:33:13 +0000

    interim push-1

--- show_client/version_cli.go
+++ show_client/version_cli.go
@@ -1,7 +1,18 @@
 package show_client
 
-import log "github.com/golang/glog"
+import (
+	log "github.com/golang/glog"
+	"time"
+)
+
+SonicVersionYamlPath = "/etc/sonic/sonic_version.yml"
 
 func getVersion() ([]byte, error) {
+	versionInfo = GetDataFromFile(SonicVersionYamlPath)
+	platformInfo = GetPlatformInfo()
+	chassisInfo = GetChassisInfo()
+
+	uptime = GetUptime()
+	sysDate = time.Now()
 	retun "empty string"
 }
