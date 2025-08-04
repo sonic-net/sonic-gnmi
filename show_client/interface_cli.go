cac8d6b23093f488d34729f4a53105baa9947db1
BYGX-wcr <wcr@live.cn>
2025-08-04 03:40:24 +0000

    merge interface_cli.go with intf_cli.go

--- show_client/interface_cli.go
+++ show_client/interface_cli.go
@@ -6,6 +6,7 @@ import (
 	log "github.com/golang/glog"
 	gnmipb "github.com/openconfig/gnmi/proto/gnmi"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -233,4 +234,63 @@ func calculateDiffSnapshot(oldSnapshot map[string]InterfaceCountersResponse, new
 		}
 	}
 	return diffResponse
+}
+
+var ALL_PORT_ERRORS = [][]string{
+	{"oper_error_status", "oper_error_status_time"},
+	{"mac_local_fault_count", "mac_local_fault_time"},
+	{"mac_remote_fault_count", "mac_remote_fault_time"},
+	{"fec_sync_loss_count", "fec_sync_loss_time"},
+	{"fec_alignment_loss_count", "fec_alignment_loss_time"},
+	{"high_ser_error_count", "high_ser_error_time"},
+	{"high_ber_error_count", "high_ber_error_time"},
+	{"data_unit_crc_error_count", "data_unit_crc_error_time"},
+	{"data_unit_misalignment_error_count", "data_unit_misalignment_error_time"},
+	{"signal_local_error_count", "signal_local_error_time"},
+	{"crc_rate_count", "crc_rate_time"},
+	{"data_unit_size_count", "data_unit_size_time"},
+	{"code_group_error_count", "code_group_error_time"},
+	{"no_rx_reachability_count", "no_rx_reachability_time"},
+}
+
+func getIntfErrors(prefix, path *gnmipb.Path) ([]byte, error) {
+	ifaces := ParseOptionsFromPath(path, "interface")
+	if len(ifaces) == 0 {
+		return nil, fmt.Errorf("No interface name specified in the path: %v", path)
+	}
+	intf := ifaces[0]
+
+	// Query Port Operational Errors Table from STATE_DB
+	queries := [][]string{
+		{"STATE_DB", "PORT_OPERR_TABLE", intf},
+	}
+	portErrorsTbl, _ := GetMapFromQueries(queries)
+	portErrorsTbl = RemapAliasToPortName(portErrorsTbl)
+
+	// Format the port errors data
+	portErrors := make([][]string, 0, len(ALL_PORT_ERRORS)+1)
+	// Append the table header
+	portErrors = append(portErrors, []string{"Port Errors", "Count", "Last timestamp(UTC)"})
+	// Iterate through all port errors types and create the result
+	for _, portError := range ALL_PORT_ERRORS {
+		count := "0"
+		timestamp := "Never"
+		if portErrorsTbl != nil {
+			if val, ok := portErrorsTbl[portError[0]]; ok {
+				count = val.(string)
+			}
+			if val, ok := portErrorsTbl[portError[1]]; ok {
+				timestamp = val.(string)
+			}
+		}
+
+		portErrors = append(portErrors, []string{
+			strings.Replace(strings.Replace(portError[0], "_", " ", -1), " count", "", -1),
+			count,
+			timestamp},
+		)
+	}
+
+	// Convert [][]string to []byte using JSON serialization
+	return json.Marshal(portErrors)
 }
