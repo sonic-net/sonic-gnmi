49d025ed2d3d9ab2bd4684c3a7858e6581c96309
zitingguo <zitingguo@microsoft.com>
2025-08-05 06:37:52 +0000

    Add show interfaces transceiver presence implementation
    Signed-off-by: zitingguo-ms <zitingguo@microsoft.com>

---
+++ show_client/interface_transceiver_presence.go
@@ -0,0 +1,55 @@
+package show_client
+
+import (
+	"encoding/json"
+	"fmt"
+
+	log "github.com/golang/glog"
+	gnmipb "github.com/openconfig/gnmi/proto/gnmi"
+)
+
+func getAllPortsFromConfigDB() ([]string, error) {
+	queries := [][]string{
+		{"CONFIG_DB", "PORT"},
+	}
+	data, err := GetMapFromQueries(queries)
+	if err != nil {
+		log.Errorf("Unable to get data from CONFIG_DB queries %v, got err: %v", queries, err)
+		return nil, err
+	}
+	ports := make([]string, 0, len(data))
+	for iface, _ := range data {
+		ports = append(ports, iface)
+	}
+	return ports, nil
+}
+
+func getInterfaceTransceiverPresence(prefix, path *gnmipb.Path) ([]byte, error) {
+	ports, error := getAllPortsFromConfigDB()
+	if error != nil {
+		log.Errorf("Unable to get all ports from DONFIG_DB, %v", error)
+		return nil, error
+	}
+
+	status := make(map[string]string)
+	queries := make([][]string, 0, len(ports))
+	for _, port := range ports {
+		queries = append(queries, []string{"STATE_DB", fmt.Sprintf("TRANSCEIVER_INFO|%s", port)})
+	}
+	log.Infof("Prepared transceiver info queries: %v", queries)
+
+	data, err := GetMapFromQueries(queries)
+	if err != nil {
+		log.Errorf("Unable to get transceiver data from STATE_DB queries %v, got err: %v", queries, err)
+		return nil, err
+	}
+
+	for _, port := range ports {
+		if _, exist := data[port]; exist {
+			status[port] = "Present"
+		} else {
+			status[port] = "Not Present"
+		}
+	}
+	return json.Marshal(status)
+}
