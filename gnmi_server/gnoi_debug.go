a93d2653dc4cf853ce0bbe4d0aaa2850771c44ab
Matt Soulsby <83053424+matthew-soulsby@users.noreply.github.com>
2025-10-06 18:00:40 +1100

    Fix endpoint

--- gnmi_server/gnoi_debug.go
+++ gnmi_server/gnoi_debug.go
@@ -1,22 +1,18 @@
 package gnmi
 
 import (
-	"context"
-
 	log "github.com/golang/glog"
 	gnoi_debug "github.com/sonic-net/sonic-gnmi/pkg/gnoi/debug"
 	gnoi_debug_pb "github.com/sonic-net/sonic-gnmi/proto/gnoi/debug"
 )
 
 func (srv *DebugServer) Debug(req *gnoi_debug_pb.DebugRequest, stream gnoi_debug_pb.Debug_DebugServer) error {
-	ctx := stream.Context()
-
 	log.Infof("GNOI Debug RPC called with request: %+v", req)
-	_, err := authenticate(srv.config, ctx, "gnoi", false)
+	_, err := authenticate(srv.config, stream.Context(), "gnoi", true)
 	if err != nil {
 		log.Errorf("authentication failed in Debug RPC: %v", err)
 		return err
 	}
 
-	return gnoi_debug.HandleCommandRequest(ctx, req, stream)
+	return gnoi_debug.HandleCommandRequest(req, stream)
 }
