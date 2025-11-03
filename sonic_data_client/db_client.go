f06dc62a5181cce3509b015f1e225dd440bdfbea
BYGX-wcr <wcr@live.cn>
2025-11-03 20:05:11 +0000

    remove err return when CountersSidMap is missing

--- sonic_data_client/db_client.go
+++ sonic_data_client/db_client.go
@@ -682,7 +682,7 @@ func populateDbtablePath(prefix, path *gnmipb.Path, pathG2S *map[*gnmipb.Path][]
 		}
 		err = initCountersSidMap()
 		if err != nil {
-			return err
+			log.Errorf("Could not create CountersSidMap: %v", err)
 		}
 	}
 
