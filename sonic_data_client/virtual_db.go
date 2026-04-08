c2e5becaf82197fe8cee1218b24b6b3c0f3bd914
Zain Budhwani <zainbudhwani@microsoft.com>
2026-04-08 00:56:19 +0000

    Remove duplicate code
    Signed-off-by: Zain Budhwani <zainbudhwani@microsoft.com>

--- sonic_data_client/virtual_db.go
+++ sonic_data_client/virtual_db.go
@@ -1126,25 +1126,6 @@ func getPortNamespace(port string) (string, error) {
 	return namespace, nil
 }
 
-func ClearMappings() {
-	value := os.Getenv("UNIT_TEST")
-	if value != "1" {
-		return
-	}
-	counterMaps := []map[string]string{
-		countersPortNameMap,
-		alias2nameMap,
-		countersFabricPortNameMap,
-		countersQueueNameMap,
-		countersAclRuleMap,
-	}
-	for _, counterMap := range counterMaps {
-		for entry := range counterMap {
-			delete(counterMap, entry)
-		}
-	}
-}
-
 func AliasToPortNameMap() map[string]string {
 	// Ensure alias map is initialized
 	initAliasMap()
